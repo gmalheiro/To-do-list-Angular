9b93ede3e363376612be4d91651a4a58b5b06412
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-12 23:12:35 -0300

    Starting theproject

---
+++ src/app/modules/home/components/to-do-input-add-itens/to-do-input-add-itens.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-input-add-itens',
+  templateUrl: './to-do-input-add-itens.component.html',
+  styleUrls: ['./to-do-input-add-itens.component.scss']
+})
+export class ToDoInputAddItensComponent {
+
+}
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Componentes
+/* import { ComponentsComponent } from './components/components.component'; */
+import { HeaderComponent } from './components/header/header.component';
+import { ToDoButtonDeleteAllComponent } from './components/to-do-button-delete-all/to-do-button-delete-all.component';
+import { ToDoInputAddItensComponent } from './components/to-do-input-add-itens/to-do-input-add-itens.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+
+//Pages 
+import { HomeComponent } from './pages/home/home.component';
+
+
+
+@NgModule({
+  declarations: [
+    //ComponentsComponent,
+    HeaderComponent,
+    ToDoButtonDeleteAllComponent,
+    ToDoInputAddItensComponent,
+    ToDoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
